6ab1827959c7119ef09b7c51152ff424f8dbff4f
cedricmills <cedric_mills@hotmail>
2022-03-26 15:17:10 -0500

    db quieres added, will work with LAMP

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+require '../src/config/db.php'
+$app = AppFactory::create();
+
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+    return $response;
+});
+
+// Customer routes 
+require '../src/routes/customers.php';
+$app->run();
---
+++ src/config/db.php
@@ -0,0 +1,17 @@
+<?php
+    class db{
+      // DB Properties
+      private $dbhost = 'localhost';
+      private $dbuser = 'cedric';
+      private $dbpass = 'password';
+      private $dbname = 'restapp';
+
+      // Connect to DB
+      public function connect() {
+        $mysql_connect_string = "mysql:host=$this->dbhost;dbname=$this->dbname";
+        $dbConnection = new PDO(mysql_connect_str, $this->dbuser, $this->dbpass);
+
+        $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $dbConnection;
+      }
+    }
